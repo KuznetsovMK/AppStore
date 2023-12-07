3806ca0a8121ffa9291ed6ba41bbe5ed7cca7b9e
Кузнецов Михаил Владимирович <Kuznetsov.M@russianpost.ru>
2023-12-07 22:08:13 +0300

    fix alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -9,6 +9,7 @@ function Alert(props) {
         return () => {
             clearTimeout(timerId);
         }
+        // eslint-disable-next-line
     }, [name])
 
     return <div id='toast-container'>
